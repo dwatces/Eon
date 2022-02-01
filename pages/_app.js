f04c5250a146d1267f44a67def5d4ff473170394
Daniel Olliver <74215535+dwatces@users.noreply.github.com>
2022-02-01 16:26:42 +1300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -11,14 +11,14 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5Q27LWP');`,
-          }}
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-219103442-1"
         ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
+          'UA-219103442-1');
+        </script>
 
         <DefaultSeo
           openGraph={{
@@ -41,7 +41,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <meta property="og:url" content="https://www.eoncandles.co.nz" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Eon Candles" />
-        <meta property="og:image" href="/public/eon-logo.png" />
+        <meta property="og:image" href="https://ibb.co/Z8JwnkT" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta
           name="viewport"
