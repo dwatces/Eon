b6fa9bef17546a945c86f37f4eb2ac8fe6b26045
Daniel Olliver <74215535+dwatces@users.noreply.github.com>
2022-02-02 10:17:37 +1300

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,22 @@
 import styles from "./Layout.module.css";
-import backgroundImg from "../public/coverPhoto.png";
+import backgroundImg from "../public/heroDesktop.webp";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => (
   <>
     <header>
-      <Image
-        src={backgroundImg}
-        alt="hero banner"
-        width={2000}
-        height={600}
-        layout="responsive"
-        className={styles.header}
-        priority
-      />
+      <Link href="/shop">
+        <Image
+          src={backgroundImg}
+          alt="hero banner"
+          width={2000}
+          height={600}
+          layout="responsive"
+          className={styles.header}
+          priority
+        />
+      </Link>
     </header>
   </>
 );
