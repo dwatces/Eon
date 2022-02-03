149649654ff655a3381d7435c6dfee43d0e4dde9
Daniel Olliver <74215535+dwatces@users.noreply.github.com>
2022-02-04 12:44:57 +1300

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import styles from "./Layout.module.css";
 import backgroundImg from "../public/heroDesktop.webp";
-import MobileLogo from "../public/hero-mobile.webp";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,18 +11,9 @@ const Header = () => (
           src={backgroundImg}
           alt="hero banner"
           width={2000}
-          height={800}
+          height={600}
           layout="responsive"
           className={styles.header}
-          priority
-        />
-      </Link>
-      <Link href="/shop" passHref>
-        <Image
-          src={MobileLogo}
-          alt=" Eon candles home mobile"
-          className={styles.mobileHeader}
-          responsive="true"
           priority
         />
       </Link>
